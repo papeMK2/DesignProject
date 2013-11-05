fc6ac892bbe09dccf224fe74c0ac916f12f778e1
papeMK2 <papemk2@gmail.com>
2013-11-06 01:56:31 +0900

    サインイン画面からページ遷移機能を追加, ルートページ変更有

---
+++ DesignProject/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DesignProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            ViewBag.Title = "DesignProject";
+            return View();
+        }
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+    }
+}
