5adb7eb1bc4f6136e874f3a33e8d3f0fa3c0963a
papeMK2 <papemk2@gmail.com>
2013-12-11 14:56:51 +0900

    EventCotroller追加

---
+++ DesignProject/Controllers/EventsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DesignProject.Controllers
+{
+    public class EventsController : Controller
+    {
+        //
+        // GET: /Events/
+
+        public ActionResult Schedule()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EventTemplate()
+        {
+            return View();
+        }
+    }
+}
